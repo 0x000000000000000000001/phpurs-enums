c868ebfec7ec29f9ab88774bf2dd1e664f5338ec
0x1 <kevin.francart3@gmail.com>
2026-07-07 17:28:48 +0200

    Add Enum PHP FFI

---
+++ src/Data/Enum.php
@@ -0,0 +1,20 @@
+<?php
+
+$toCharCode = function($c) {
+    if (function_exists('mb_ord')) {
+        return \mb_ord($c, "UTF-8");
+    }
+    // Very rudimentary fallback
+    return \ord($c);
+};
+
+$fromCharCode = function($c) {
+    if (function_exists('mb_chr')) {
+        return \mb_chr($c, "UTF-8");
+    }
+    return \chr($c);
+};
+
+$exports['toCharCode'] = $toCharCode;
+$exports['fromCharCode'] = $fromCharCode;
+return $exports;
